0432eb31b1d7bd966db41fe1268bc493e3272c67
Gururaj Sudhindra <gururaj.sudhindra@gmail.com>
2025-03-03 05:43:27 -0500

    create API for search service

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const cors = require('cors');
 const pinoLogger = require('./logger');
 
+
 const connectToDatabase = require('./models/db');
 const {loadData} = require("./util/import-mongo/index");
 
@@ -28,6 +29,7 @@ app.use(express.json());
 const giftRoutes = require('./routes/giftRoutes');
 // Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
 //{{insert code here}}
+const searchRoutes = require('./routes/searchRoutes');
 
 
 const pinoHttp = require('pino-http');
@@ -38,6 +40,7 @@ app.use(pinoHttp({ logger }));
 // Use Routes
 // Gift API Task 2: add the giftRoutes to the server by using the app.use() method.
 //{{insert code here}}
+app.use('/api/search', searchRoutes);
 app.use('/api/gifts', giftRoutes);
 // Search API Task 2: add the searchRoutes to the server by using the app.use() method.
 //{{insert code here}}
