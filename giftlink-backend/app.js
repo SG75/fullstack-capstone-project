e86728f43dc81428684fea1a430de32f5ab9e442
Gururaj Sudhindra <gururaj.sudhindra@gmail.com>
2025-03-07 03:01:03 -0500

    create register and login pages

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -10,7 +10,29 @@ const router = express.Router(); // Initialize router
 
 const connectToDatabase = require('./models/db');
 const {loadData} = require("./util/import-mongo/index");
-
+import React from 'react';
+import { Routes, Route, useNavigate } from 'react-router-dom';
+import MainPage from './components/MainPage/MainPage';
+import LoginPage from './components/LoginPage/LoginPage';
+import RegisterPage from './components/RegisterPage/RegisterPage';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import Navbar from './components/Navbar/Navbar';
+function App() {
+  const navigate = useNavigate();
+  return (
+        <>
+        <Navbar/>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/app" element={<MainPage />} />
+          <Route path="/app/login" element={<LoginPage/>} />
+          <Route path="/app/register" element={<RegisterPage />} />
+        </Routes>
+        </>
+  );
+}
+export default App;
 
 
 const app = express();
