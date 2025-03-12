711ea2cb14ead22c508720403dc3055f87cfa5db
Gururaj Sudhindra <gururaj.sudhindra@gmail.com>
2025-03-12 11:21:40 +0530

    fix linting error

--- giftlink-frontend/src/components/Navbar/Navbar.js
+++ giftlink-frontend/src/components/Navbar/Navbar.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
-import { Link, useNavigate } from "react-router-dom";
+// import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 // import { urlConfig } from "../../config";
 import { useAppContext } from "../../context/AuthContext";
 
