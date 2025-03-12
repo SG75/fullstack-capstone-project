bf67d7017bb1184bb8b45d08f536fced033d0699
Gururaj Sudhindra <gururaj.sudhindra@gmail.com>
2025-03-13 05:14:15 +0530

    correct linting errors

--- giftlink-frontend/src/components/RegisterPage/RegisterPage.js
+++ giftlink-frontend/src/components/RegisterPage/RegisterPage.js
@@ -9,7 +9,8 @@ function RegisterPage() {
   const [lastName, setLastName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [showerr, setShowerr] = useState("");
+  // const [showerr, setShowerr] = useState("");
+  const [setShowerr] = useState("");
   const navigate = useNavigate();
   const { setIsLoggedIn } = useAppContext();
   const handleRegister = async () => {
